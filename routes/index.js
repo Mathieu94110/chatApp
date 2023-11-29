fbf0dd53a51e359a584f270d9ede4f5a177d73d3
mathieu enault <me94110@gmail.com>
2023-11-29 19:51:58 +0100

    adding navbar and styling improvement

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,13 @@
-const userRoutes = require('./user.routes');
-const authRoutes = require('./auth.routes');
-const router = require('express').Router();
-const { ensureAuthenticated } = require('../config/security.config');
+const userRoutes = require("./user.routes");
+const authRoutes = require("./auth.routes");
+const router = require("express").Router();
 
-router.use('/users', userRoutes);
-router.use('/auth', authRoutes);
-
-router.get('/protected', ensureAuthenticated, (req, res) => {
-  res.render('protected');
-})
-
-router.get('/',  (req, res) => {
-  res.render('index', { user: req.user });
+router.use("/users", userRoutes);
+router.use("/auth", authRoutes);
+router.get("/", (req, res) => {
+  req.isAuthenticated()
+    ? res.render("index", { user: req.user })
+    : res.redirect("/auth/signin/form");
 });
 
 module.exports = router;
